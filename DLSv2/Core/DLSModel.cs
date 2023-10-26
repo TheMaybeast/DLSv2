99bd9190a00f1c2ac3f14a0fc1fea9c814b673a1
TheMaybeast <59030276+TheMaybeast@users.noreply.github.com>
2023-10-26 00:56:49 -0300

    Adds sample VCF

---
+++ DLSv2/Core/DLSModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DLSv2.Core
+{
+    [XmlRoot("Model")]
+    public class DLSModel
+    {
+        [XmlElement("SoundSettings")]
+        public SoundSettings SoundSettings;
+
+        [XmlElement("LightStages")]
+        public List<LightStage> LightStages;
+    }
+
+    public class SoundSettings
+    {
+        [XmlElement("Tones")]
+        public string Tones;
+
+        [XmlElement("Horn")]
+        public string Horn = "sirens_airhorn";
+
+        [XmlElement("AirHornInterruptsSiren")]
+        public string AirHornInterruptsSiren = "false";
+
+        [XmlIgnore]
+        public List<string> SirenTones = new List<string> { "sirens_slow_dir", "fast_9mvv0vf", "", "" };
+    }
+
+    public class LightStage
+    {
+        [XmlElement("Yield")]
+        public string Yield = "true";
+
+        [XmlElement("Extras")]
+        public List<Extra> Extra;
+
+        [XmlElement("SirenSetting")]
+        public SirenSetting SirenSetting;
+    }
+
+    public class Extra
+    {
+        [XmlAttribute("ID")]
+        public string ID;
+
+        [XmlAttribute("Enabled")]
+        public string Enabled;
+    }
+}
