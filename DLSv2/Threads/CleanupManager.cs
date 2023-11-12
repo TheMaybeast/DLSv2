d2921fd38c328ff9e7ed30b76bb09f41c7c8cf5f
TheMaybeast <59030276+TheMaybeast@users.noreply.github.com>
2023-11-12 17:09:08 -0300

    Optimize Cleanup Manager

--- DLSv2/Threads/CleanupManager.cs
+++ DLSv2/Threads/CleanupManager.cs
@@ -4,6 +4,7 @@ using Rage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 
 namespace DLSv2.Threads
 {
@@ -17,28 +18,30 @@ namespace DLSv2.Threads
             while (true)
             {
                 int checksDone = 0;
-                List<uint> ManagedVehicles = new List<uint>();
 
                 foreach (ManagedVehicle activeVeh in Entrypoint.ManagedVehicles.ToList())
                 {
                     if (!activeVeh.Vehicle)
+                    {
+                        // Removes from Managed Vehicles
                         Entrypoint.ManagedVehicles.Remove(activeVeh);
-                    else
-                        ManagedVehicles.Add(activeVeh.VehicleHandle);
+
+                        // Adds EL to available pool, if used
+                        if (Entrypoint.ELUsedPool.ContainsKey(activeVeh.VehicleHandle))
+                        {
+                            ("Moving " + activeVeh.VehicleHandle + " to Available Pool").ToLog();
+                            Entrypoint.ELAvailablePool.Add(Entrypoint.ELUsedPool[activeVeh.VehicleHandle]);
+                            Entrypoint.ELUsedPool.Remove(activeVeh.VehicleHandle);
+                        }
+
+                        // Clears all sound IDs
+                        foreach (var soundId in activeVeh.SoundIds)
+                            Audio.StopMode(activeVeh, soundId.Key);
+                    }
 
                     checksDone++;
                     if (checksDone % yieldAfterChecks == 0)
                         GameFiber.Yield();
-                }
-
-                foreach (uint hash in Entrypoint.ELUsedPool.Keys.ToList())
-                {
-                    if (!ManagedVehicles.Contains(hash))
-                    {
-                        ("Moving " + hash + " to Available Pool").ToLog();
-                        Entrypoint.ELAvailablePool.Add(Entrypoint.ELUsedPool[hash]);
-                        Entrypoint.ELUsedPool.Remove(hash);
-                    }
                 }
                 GameFiber.Sleep((int)Math.Max(timeBetweenChecks, Game.GameTime - lastProcessTime));
                 lastProcessTime = Game.GameTime;
