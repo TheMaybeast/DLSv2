298aed7639d5d3f47e7d399d83a905d923ac026c
pnwparksfan <gtaparks@gmail.com>
2023-12-11 02:24:30 -0800

    Added random chance condition, evaluated once at spawn

---
+++ DLSv2/Conditions/RandomConditions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DLSv2.Conditions
+{
+    using Core;
+    using Rage;
+    using System.Xml.Serialization;
+
+    public class RandomCondition : VehicleCondition
+    {
+        private static Dictionary<ManagedVehicle, bool> cachedResults = new Dictionary<ManagedVehicle, bool>();
+
+        [XmlAttribute("chance")]
+        public int PercentChance { get; set; }
+
+        public bool GetRandomResult() => PercentChance >= MathHelper.GetRandomInteger(0, 100);
+
+        protected override bool Evaluate(ManagedVehicle veh)
+        {
+            if (!cachedResults.TryGetValue(veh, out bool result))
+            {
+                result = GetRandomResult();
+                cachedResults.Add(veh, result);
+            }
+
+            return result;
+        }
+    }
+}
