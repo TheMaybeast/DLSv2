070c75204a5aa450a613f1c671620dc41bdbaea6
TheMaybeast <59030276+TheMaybeast@users.noreply.github.com>
2024-01-25 06:18:56 -0300

    Fixes audio logging level

--- DLSv2/Utils/Game/Audio.cs
+++ DLSv2/Utils/Game/Audio.cs
@@ -11,7 +11,7 @@ public static class Audio
     public static int PlaySoundFromEntity(this Entity entity, string audioName, string audioRef)
     {
         int newID = NativeFunction.Natives.GET_SOUND_ID<int>();
-        ("Allocated Sound ID ["+newID+"]").ToLog();
+        ("Allocated Sound ID ["+newID+"]").ToLog(LogLevel.DEBUG);
         usedSoundIds.Add(newID);
         NativeFunction.Natives.PLAY_SOUND_FROM_ENTITY(newID, audioName, entity, audioRef, 0, 0);
         return newID;
@@ -21,7 +21,7 @@ public static class Audio
     {
         NativeFunction.Natives.STOP_SOUND(soundId);
         NativeFunction.Natives.RELEASE_SOUND_ID(soundId);
-        ("Released Sound ID [" + soundId + "]").ToLog();
+        ("Released Sound ID [" + soundId + "]").ToLog(LogLevel.DEBUG);
         usedSoundIds.Remove(soundId);
     }
 }
